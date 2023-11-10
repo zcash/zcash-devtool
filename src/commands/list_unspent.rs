9a5b97898b33b4e6d8050cacbe10d35cc5ac8bb2
Jack Grigg <jack@electriccoin.co>
2023-11-10 04:23:52 +0000

    list-unspent: Migrate to `select_spendable_sapling_notes` API
    `get_spendable_sapling_notes` will be removed in the next release.

--- src/commands/list_unspent.rs
+++ src/commands/list_unspent.rs
@@ -3,9 +3,13 @@ use gumdrop::Options;
 
 use zcash_client_backend::data_api::WalletRead;
 use zcash_client_sqlite::WalletDb;
-use zcash_primitives::{consensus::Parameters, zip32::AccountId};
+use zcash_primitives::{
+    consensus::Parameters,
+    transaction::components::amount::{Amount, MAX_MONEY},
+    zip32::AccountId,
+};
 
-use crate::{data::get_db_paths, error, ui::format_zec, MIN_CONFIRMATIONS};
+use crate::{data::get_db_paths, error, ui::format_zec};
 
 // Options accepted for the `balance` command
 #[derive(Debug, Options)]
@@ -21,12 +25,20 @@ impl Command {
         let (_, db_data) = get_db_paths(wallet_dir);
         let db_data = WalletDb::for_path(db_data, params)?;
 
-        let (target_height, _) = db_data
-            .get_target_and_anchor_heights(MIN_CONFIRMATIONS)?
+        // Use the height of the maximum scanned block as the anchor height, to emulate a
+        // zero-conf transaction in order to select every note in the wallet.
+        let anchor_height = db_data
+            .block_max_scanned()?
             .ok_or(error::WalletErrorT::ScanRequired)
-            .map_err(|e| anyhow!("{:?}", e))?;
+            .map_err(|e| anyhow!("{:?}", e))?
+            .block_height();
 
-        let notes = db_data.get_spendable_sapling_notes(account, target_height, &[])?;
+        let notes = db_data.select_spendable_sapling_notes(
+            account,
+            Amount::const_from_i64(MAX_MONEY),
+            anchor_height,
+            &[],
+        )?;
 
         for note in notes {
             println!("{}: {}", note.note_id, format_zec(note.note_value));
