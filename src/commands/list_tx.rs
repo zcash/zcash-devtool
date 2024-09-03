455b492bd3009296cc21cfedde8b2b2ba93253fc
Kris Nuttycombe <kris@nutty.land>
2024-09-03 17:14:12 -0600

    Print per-output information and memos in `list-tx` output.

--- src/commands/list_tx.rs
+++ src/commands/list_tx.rs
@@ -9,6 +9,10 @@ use zcash_primitives::{
         TxId,
     },
 };
+use zcash_protocol::{
+    memo::{Memo, MemoBytes},
+    PoolType,
+};
 
 use crate::{data::get_db_paths, ui::format_zec};
 
@@ -63,13 +67,44 @@ impl Command {
                 sort_height",
         )?;
 
+        let mut stmt_outputs = conn.prepare(
+            "SELECT
+                output_pool,
+                output_index,
+                from_account_id,
+                to_account_id,
+                to_address,
+                value,
+                is_change,
+                memo
+             FROM v_tx_outputs
+             WHERE txid = :txid",
+        )?;
+
         println!("Transactions:");
         for row in stmt_txs.query_and_then(
             named_params! {":account_id": account_id},
             |row| -> anyhow::Result<_> {
+                let txid = row.get::<_, Vec<u8>>(1)?;
+
+                let tx_outputs = stmt_outputs
+                    .query_and_then(named_params![":txid": txid], |out_row| {
+                        WalletTxOutput::new(
+                            out_row.get(0)?,
+                            out_row.get(1)?,
+                            out_row.get(2)?,
+                            out_row.get(3)?,
+                            out_row.get(4)?,
+                            out_row.get(5)?,
+                            out_row.get(6)?,
+                            out_row.get(7)?,
+                        )
+                    })?
+                    .collect::<Result<Vec<_>, _>>()?;
+
                 WalletTx::from_parts(
                     row.get(0)?,
-                    row.get(1)?,
+                    txid,
                     row.get(2)?,
                     row.get(3)?,
                     row.get(4)?,
@@ -78,6 +113,7 @@ impl Command {
                     row.get(7)?,
                     row.get(8)?,
                     row.get(9)?,
+                    tx_outputs,
                 )
             },
         )? {
@@ -87,6 +123,87 @@ impl Command {
         }
 
         Ok(())
+    }
+}
+
+struct WalletTxOutput {
+    pool: PoolType,
+    output_index: u32,
+    from_account: Option<i64>,
+    to_account: Option<i64>,
+    to_address: Option<String>,
+    value: NonNegativeAmount,
+    is_change: bool,
+    memo: Option<Memo>,
+}
+
+impl WalletTxOutput {
+    fn parse_pool_code(pool_code: i64) -> Option<PoolType> {
+        match pool_code {
+            0 => Some(PoolType::Transparent),
+            2 => Some(PoolType::SAPLING),
+            3 => Some(PoolType::ORCHARD),
+            _ => None,
+        }
+    }
+
+    fn new(
+        pool_code: i64,
+        output_index: u32,
+        from_account: Option<i64>,
+        to_account: Option<i64>,
+        to_address: Option<String>,
+        value: i64,
+        is_change: bool,
+        memo: Option<Vec<u8>>,
+    ) -> Result<Self, anyhow::Error> {
+        Ok(Self {
+            pool: Self::parse_pool_code(pool_code)
+                .ok_or(anyhow!("Pool code not recognized: {}", pool_code))?,
+            output_index,
+            from_account,
+            to_account,
+            to_address,
+            value: NonNegativeAmount::from_nonnegative_i64(value)?,
+            is_change,
+            memo: memo
+                .as_ref()
+                .map(|b| MemoBytes::from_bytes(b).and_then(Memo::try_from))
+                .transpose()
+                .map_err(|e| anyhow!("{}", e))?,
+        })
+    }
+
+    fn print(&self) {
+        println!("  Output {} ({})", self.output_index, self.pool);
+        println!(
+            "    Value: {}{}",
+            format_zec(self.value),
+            if self.is_change {
+                " (Change)"
+            } else if self.from_account.is_some() && self.to_account.is_some() {
+                " (Wallet Internal Transfer)"
+            } else {
+                ""
+            }
+        );
+
+        if !(self.from_account.is_some() && self.to_account.is_some()) {
+            if let Some(account_id) = self.to_account {
+                println!("    Received by account: {}", account_id);
+            }
+            if let Some(account_id) = self.from_account {
+                println!("    Sent from account: {}", account_id);
+            }
+        }
+
+        if let Some(addr) = &self.to_address {
+            println!("    To: {}", addr);
+        }
+
+        if let Some(memo) = &self.memo {
+            println!("    Memo: {:?}", memo);
+        }
     }
 }
 
@@ -101,6 +218,7 @@ struct WalletTx {
     memo_count: usize,
     block_time: Option<i64>,
     expired_unmined: bool,
+    outputs: Vec<WalletTxOutput>,
 }
 
 impl WalletTx {
@@ -116,6 +234,7 @@ impl WalletTx {
         memo_count: usize,
         block_time: Option<i64>,
         expired_unmined: bool,
+        outputs: Vec<WalletTxOutput>,
     ) -> anyhow::Result<Self> {
         Ok(WalletTx {
             mined_height: mined_height.map(BlockHeight::from_u32),
@@ -131,6 +250,7 @@ impl WalletTx {
             memo_count,
             block_time,
             expired_unmined,
+            outputs,
         })
     }
 
@@ -169,5 +289,8 @@ impl WalletTx {
             "  Sent {} notes, received {} notes, {} memos",
             self.sent_note_count, self.received_note_count, self.memo_count,
         );
+        for output in &self.outputs {
+            output.print()
+        }
     }
 }
