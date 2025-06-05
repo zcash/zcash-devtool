b23334484096f8eb613925a6e528255acc7efd01
dismad <81990132+dismad@users.noreply.github.com>
2025-06-04 23:10:41 -0700

    Update list_addresses.rs
    Display sapling, Sapling + Orchard, Full UA addresses

--- src/commands/wallet/list_addresses.rs
+++ src/commands/wallet/list_addresses.rs
@@ -2,8 +2,11 @@ use clap::Args;
 use uuid::Uuid;
 use zcash_client_backend::data_api::Account;
 use zcash_client_sqlite::WalletDb;
-use zcash_keys::keys::UnifiedAddressRequest;
-
+use zcash_keys::{keys::UnifiedAddressRequest};
+use zcash_client_backend::encoding::encode_payment_address;
+use zcash_primitives::consensus::MainNetwork;
+use zcash_protocol::consensus::NetworkConstants;
+use zcash_client_backend::address::UnifiedAddress;
 use crate::{commands::select_account, config::get_wallet_network, data::get_db_paths};
 
 #[cfg(feature = "qr")]
@@ -26,18 +29,54 @@ impl Command {
         let params = get_wallet_network(wallet_dir.as_ref())?;
         let (_, db_data) = get_db_paths(wallet_dir.as_ref());
         let db_data = WalletDb::for_path(db_data, params, (), ())?;
-
         let account = select_account(&db_data, self.account_id)?;
 
-        println!("Account {:?}", account.id());
+        println!("\nAccount        : {:?}\n\n", account.id());
+       
         let (ua, _) = account
             .uivk()
             .default_address(UnifiedAddressRequest::AllAvailableKeys)?;
+            
         let ua_str = ua.encode(&params);
-        println!("     Default Address: {}", ua_str);
+
+        let sapling_test = UnifiedAddress::sapling(&ua).unwrap();
+        let mysap: String = encode_payment_address(MainNetwork.hrp_sapling_payment_address(), &sapling_test);
+  
+        let orchard_test = UnifiedAddress::orchard(&ua).unwrap();
+
+        // Build UA Recievers
+        let orchard_address = UnifiedAddress::from_receivers(Some(*orchard_test), Some(sapling_test.clone()), None).unwrap();
+        let orchard_sapling_address_str = orchard_address.encode(&MainNetwork);
+
+       
+        let mysapling: String = ToString::to_string(&mysap);
+        let mysap_orch: String = ToString::to_string(&orchard_sapling_address_str);
+        let myfull_ua: String = ToString::to_string(&ua_str);
+
 
         #[cfg(feature = "qr")]
         if self.display_qr {
+            let code = QrCode::new(mysap)?;
+            let ua_qr = code
+                .render::<unicode::Dense1x2>()
+                .dark_color(unicode::Dense1x2::Light)
+                .light_color(unicode::Dense1x2::Dark)
+                .quiet_zone(true)
+                .build();
+            println!("{}\n", ua_qr);
+            println!("Sapling Address: {}\n", mysapling);
+
+
+            let code = QrCode::new(orchard_sapling_address_str)?;
+            let ua_qr = code
+                .render::<unicode::Dense1x2>()
+                .dark_color(unicode::Dense1x2::Light)
+                .light_color(unicode::Dense1x2::Dark)
+                .quiet_zone(true)
+                .build();
+            println!("{}\n", ua_qr);
+            println!("Orchard + Sapling UA: {}\n", mysap_orch);
+
             let code = QrCode::new(ua_str)?;
             let ua_qr = code
                 .render::<unicode::Dense1x2>()
@@ -45,9 +84,9 @@ impl Command {
                 .light_color(unicode::Dense1x2::Dark)
                 .quiet_zone(true)
                 .build();
-            println!("{}", ua_qr);
+            println!("{}\n", ua_qr);
+            println!("Default Address: {}\n", myfull_ua);
         }
-
         Ok(())
     }
 }
