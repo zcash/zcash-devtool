643f2713406dfebb367129d1c345f23998b27788
al amoda <alamoda@tutamail.com>
2026-02-26 08:52:42 -0500

    add  to load_image.sh

--- utils/load_image.sh
+++ utils/load_image.sh
@@ -4,15 +4,17 @@ set -e
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 OCI_OUTPUT="$REPO_ROOT/build/oci"
+TARBALL="${OCI_OUTPUT}/zcash-devtool.tar"
+
 
 # Build runtime image for docker run
 echo "Checking if the OCI output from build is present."
-if [ -f $OCI_OUTPUT/zcash-devtool.tar ];
+if [ -f "$TARBALL" ];
 then
-  echo "OCI output file not present."
-else
   echo "OCI output file present, loading tar file into local docker image store."
-  docker load < $OCI_OUTPUT/zcash-devtool.tar
+  docker load < $TARBALL
   echo "...Done!"
+else
+  echo "OCI output file not present."
 fi
 
